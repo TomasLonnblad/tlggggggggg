ed0425dd6e759c6d04f32cc541d5282955290e2d
TomasLonnblad <tomaslonnblad@gmail.com>
2025-09-08 07:47:25 +0200

    x

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/dist/ScrollSmoother';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { Inter } from 'next/font/google';
+import { useGSAP } from '@gsap/react';
+
+import Header from '@/components/Header';
+
+import './globals.css';
+
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+}
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  useGSAP(
+    () => {
+      ScrollSmoother.create({
+        smooth: 2,
+        effects: true,
+      });
+    },
+    {
+      dependencies: [pathname],
+      revertOnUpdate: true,
+    }
+  );
+
+  return (
+    <html lang="en">
+      <head />
+      <body className={inter.className}>
+        <Header />
+        <div id="smooth-wrapper">
+          <div id="smooth-content">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+const HomePage = () => {
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger);
+      ScrollTrigger.create({
+        trigger: '.box-c',
+        pin: true,
+        start: 'center center',
+        end: '+=300',
+        markers: true,
+      });
+    },
+    {
+      scope: container,
+    }
+  );
+
+  return (
+    <main className="home" ref={container}>
+      <div className="header">
+        <h1 className="title">ScrollSmoother NextJS App Folder</h1>
+        <p>
+          Simple example for setting up GSAP ScrollSmoother in a NextJS App
+          using the{' '}
+          <strong>
+            <i>App</i>
+          </strong>{' '}
+          Router
+        </p>
+      </div>
+      <div className="box box-a gradient-blue" data-speed="0.5">
+        a
+      </div>
+      <div className="box box-b gradient-orange" data-speed="0.8">
+        b
+      </div>
+      <div className="box box-c gradient-purple" data-speed="1.5">
+        c
+      </div>
+      <div className="line"></div>
+    </main>
+  );
+};
+
+export default HomePage;
